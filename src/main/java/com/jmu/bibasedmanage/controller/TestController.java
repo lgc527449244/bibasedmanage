fc5dc43e038e49d4444d2efc76666ec5285a07e8
liujc <527449244@qq.com>
2017-12-20 21:38:03 +0800

    (gitignore commit and remove .idea)

--- src/main/java/com/jmu/bibasedmanage/controller/TestController.java
+++ src/main/java/com/jmu/bibasedmanage/controller/TestController.java
@@ -1,8 +1,14 @@
 package com.jmu.bibasedmanage.controller;
 
+import com.jmu.bibasedmanage.util.ResponseUtil;
+import com.jmu.bibasedmanage.vo.JsonResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ljc on 2017/12/14.
@@ -14,5 +20,14 @@ public class TestController {
     @RequestMapping("/index")
     public ModelAndView test(){
         return new ModelAndView("test.html");
+    }
+    @RequestMapping("/data")
+    @ResponseBody
+    public JsonResponse data(){
+        String key1 = "test";
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("ss","111");
+        map.put("te","22");
+        return ResponseUtil.successMap(key1, map);
     }
 }
